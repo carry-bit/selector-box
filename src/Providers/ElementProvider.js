5ea91b4f1827279fbab84e40e2b5656a694eac51
Sajjad <real.fisae@gmail.com>
2021-01-10 01:21:30 +0330

    Ready to implement ElementProvider class

---
+++ src/Providers/ElementProvider.js
@@ -0,0 +1,11 @@
+import Element from '../Objects/Element';
+
+class ElementProvider {
+    constructor(_hostElement, _reference) {
+        this._hostElement = _hostElement;
+        this._reference = _reference;
+    }
+}
+
+
+export default ElementProvider;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import Element from './Objects/Element';
+import ElementProvider from './Providers/ElementProvider';
 import {
     validateOptions,
     getHost
@@ -7,7 +7,14 @@ import {
 export function init(_options) {
     // Error handling
     validateOptions(_options);
-    
+
+    // get host element for selectbox
     const _hostElementId = _options.el;
     const _hostElement = getHost(_hostElementId);
+
+    // get reference
+    const _references = _options.ref;
+
+
+    const _elementProvider = new ElementProvider(_hostElement, _references);
 }
