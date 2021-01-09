e71ad17a397e23860cfc802c6638ce3f277c2fee
Sajjad <real.fisae@gmail.com>
2021-01-10 00:47:27 +0330

    error handling in index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 import Element from './Objects/Element';
 
 export function init(_options) {
-    
+    // Error handling
+    if(_options === undefined)
+        throw Error('You should pass an Option object to init function call.');
+    else if(typeof _options !== 'object')
+        throw Error('Unvalid Option object passed to init function call.');
+    else if(_options.el === undefined)
+        throw Error('Option object should have a (el) property.');
+
+    const _hostElement = _options.el;
 }
